ba3562d3b5babcd14c0113dfa6fe52a454f3bb54
Lucas Santos <lukas.gabriel999@gmail.com>
2024-01-15 09:51:00 -0300

    CSS adjusts

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem na tag.
+  if (html.classList.contains("light")) {
+    //Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Lucas Gabriel de terno bege e gravata terracota"
+    )
+  } else {
+    //se tiver sem light mode, manter a imagem atual
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Lucas Gabriel de terno bege e gravata terracota"
+    )
+  }
+}
